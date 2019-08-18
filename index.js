55de414b8ab155af5e212a77721fabfe3b32afaf
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-18 17:54:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,69 +1,66 @@
-let driverId=0;
-let passengerId=0;
-let tripId=0;
 let store = { drivers: [], passengers: [], trips: [] };
 
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
 
 class Driver {
-    constructor(name){
-        this.name=name;
-        this.id=++driverId;
+  constructor(name) {
+    this.name = name;
+    this.id = driverId++;
     store.drivers.push(this);
   }
-  
-         trips() {return store.trips.filter(
-            function(item) {
-                return item.driverId === this.id;
-            }.bind(this)
-        );
-       }
-          passengers() {return store.passengers.find(
-            function(item) {
-                return item.id === this.passengerId;
-            }.bind(this)
-            );
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id;
+    });
+  }
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger();
+    });
+  }
+}
 
-           }
-         }
+
 class Passenger {
-     constructor(name){
-         this.name=name;
-         this.id=++passengerId;
+  constructor(name) {
+    this.name = name;
+    this.id = passengerId++;
     store.passengers.push(this);
   }
 
-   trips() {return store.trips.filter(
-            function(item) {
-                return item.passengerId === this.id;
-            }.bind(this)
-        );
+   trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId == this.id;
+    });
+  }
+  
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver();
+    });
+  }
 }
 
-        drivers() {
-
-         }
-        }
 
 class Trip {
-  constructor(driver,passenger){
-         this.driverId=driver;
-         this.passengerId=passenger;
-         this.id=++tripId;
-         this.passengerId=passenger.id;
-         this.driverId=driver.id;
+  constructor(driver, passenger) {
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = tripId++;
     store.trips.push(this);
   }
-
-       driver(){return store.trips.filter(
-            function(item) {
-                return item.driverId === this.id;
-            }.bind(this)
-               );
-
-              }
-
-        passengers(){
-
-          }
   
-}
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+  
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
+} 
